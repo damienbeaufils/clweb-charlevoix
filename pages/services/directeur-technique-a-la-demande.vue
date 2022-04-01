525fb43888bb27d68d0ea6bffcb2c77552fee090
Damien Beaufils <damien.beaufils@gmail.com>
2022-04-01 11:51:56 -0400

    💬 enhance CTO-as-a-Service page

--- pages/services/directeur-technique-a-la-demande.vue
+++ pages/services/directeur-technique-a-la-demande.vue
@@ -20,17 +20,79 @@
               <base-text class="mt-5">
                 Vous dirigez une compagnie, vous avez besoin d’une personne sur
                 le long terme pour vous aider à
-                <b>prendre des décisions relatives aux TI</b> mais vous n’avez
-                pas le budget pour embaucher une personne qui aurait le rôle de
-                directeur technique permanent à temps plein ?
+                <b
+                  >prendre des décisions relatives aux Technologies de
+                  l'Information (TI)</b
+                >
+                mais vous n’avez pas le budget pour embaucher une personne qui
+                aurait le rôle de directeur technique permanent à temps plein ?
               </base-text>
 
               <base-text class="mt-5">
-                Qu’il s’agisse de quelques heures par semaine ou simplement de 1
-                journée par mois, nous pouvons vous accompagner en occupant le
-                rôle de directeur technique à temps partiel afin de
+                Qu’il s’agisse de quelques heures par semaine ou simplement
+                d'une journée par mois, nous pouvons vous accompagner en
+                occupant le rôle de directeur technique à temps partiel afin de
                 <b>vous aider à piloter les ressources TI de votre compagnie</b
                 >.
+              </base-text>
+
+              <base-text class="mt-5">
+                Nous pouvons par exemple vous accompagner dans :
+                <ul>
+                  <li>le <b>pilotage de votre budget</b> TI annuel,</li>
+                  <li>la <b>gestion du risque</b> et de vos contraintes,</li>
+                  <li>
+                    la <b>prise de décisions</b> structurelles pour votre
+                    compagnie,
+                  </li>
+                  <li>l'implantation de nouveaux <b>processus TI</b>,</li>
+                  <li>
+                    la construction d'un parcours de
+                    <b>formation TI</b> interne,
+                  </li>
+                  <li>
+                    la construction d'une <b>politique de sécurité</b> de votre
+                    système d'information,
+                  </li>
+                  <li>
+                    le stockage et la
+                    <b>gestion de vos données</b> d'entreprise,
+                  </li>
+                  <li>le choix d'une <b>infrastructure infonuagique</b>,</li>
+                  <li>
+                    le <b>choix d'outils ou de logiciels</b> utilisés par vos
+                    collaborateurs,
+                  </li>
+                  <li>
+                    le <b>recrutement</b> d'un Directeur Technique, de
+                    développeurs ou de toute autre personne rattachée aux TI,
+                  </li>
+                  <li>
+                    la <b>réalisation d'appels d'offres</b> pour soumettre des
+                    demandes sur des plateformes telles que SEAO,
+                  </li>
+                  <li>
+                    le <b>choix de prestataires externes</b> qui répondraient à
+                    vos appels d'offres,
+                  </li>
+                  <li>la gestion de la <b>dette technique</b>.</li>
+                </ul>
+              </base-text>
+
+              <base-text class="mt-5">
+                Pour identifier correctement votre besoin, nous aurons besoin
+                d'une première rencontre afin que vous nous exposiez vos
+                problématiques et vos contraintes. Suite à cette rencontre, nous
+                pourrons vous faire une
+                <b
+                  >proposition d'accompagnement correspondant à votre budget et
+                  votre réalité</b
+                >.
+              </base-text>
+              <base-text class="mt-5">
+                Une fois la proposition acceptée, l'accompagnement peut aussi
+                bien se faire à distance avec des outils collaboratifs virtuels,
+                que sur place dans les locaux de votre compagnie.
               </base-text>
 
               <div class="text-center">
