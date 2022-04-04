449a04c0f42889e2a50bf9c187012aa90a6560f1
Damien Beaufils <damien.beaufils@gmail.com>
2022-04-04 10:34:33 -0400

    🐛 disable google fonts nuxt module
    While waiting https://github.com/nuxt-community/google-fonts-module/issues/78 to be fixed

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,6 +44,17 @@ export default {
       },
       { rel: 'manifest', href: '/site.webmanifest' },
       { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossorigin: true,
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
+      },
     ],
     script: [
       {
@@ -70,17 +81,17 @@ export default {
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
     // https://google-fonts.nuxtjs.org/
-    [
-      '@nuxtjs/google-fonts',
-      {
-        families: {
-          Roboto: {
-            ital: [100, 300, 400, 500, 700, 900],
-            wght: [100, 300, 400, 500, 700, 900],
-          },
-        },
-      },
-    ],
+    // [
+    //   '@nuxtjs/google-fonts',
+    //   {
+    //     families: {
+    //       Roboto: {
+    //         ital: [100, 300, 400, 500, 700, 900],
+    //         wght: [100, 300, 400, 500, 700, 900],
+    //       },
+    //     },
+    //   },
+    // ],
     'nuxt-compress',
   ],
 
