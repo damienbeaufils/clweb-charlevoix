baa64ac4f14ede38750816e171a2bffd5693271b
Damien Beaufils <damien.beaufils@gmail.com>
2022-07-17 16:13:11 -0400

    🐛 temp fix to handle english after form submission

--- pages/merci.vue
+++ pages/merci.vue
@@ -1,9 +1,9 @@
 <template>
   <v-container fluid class="pa-0">
-    <section id="team" class="overflow-hidden">
+    <section id="thank-you" class="overflow-hidden">
       <v-row no-gutters>
         <v-col
-          class="align-content-space-between layout wrap"
+          class="align-content-space-between wrap"
           cols="12"
           md="8"
           :pa-5="$vuetify.breakpoint.smAndDown"
@@ -11,19 +11,40 @@
           <v-row align="center" justify="center">
             <v-col cols="10">
               <base-subheading class="mt-15 mb-10"
-                ><span v-html="$t('thank-you.subheading')"></span
+                ><span v-html="$t('thank-you.subheading', 'fr')"></span
               ></base-subheading>
               <base-text class="mb-10"
-                ><span v-html="$t('thank-you.paragraph')"></span
+                ><span v-html="$t('thank-you.paragraph', 'fr')"></span
               ></base-text>
 
               <div class="text-center">
                 <base-btn
                   nuxt
-                  :to="localePath('/')"
+                  :to="localePath('/', 'fr')"
                   class="mt-5 mb-10 text-h6 px-7 py-7"
                 >
-                  <span v-html="$t('thank-you.back')"></span>
+                  <span v-html="$t('thank-you.back', 'fr')"></span>
+                </base-btn>
+              </div>
+            </v-col>
+          </v-row>
+
+          <v-row align="center" justify="center">
+            <v-col cols="10">
+              <base-subheading class="mt-15 mb-10"
+                ><span v-html="$t('thank-you.subheading', 'en')"></span
+              ></base-subheading>
+              <base-text class="mb-10"
+                ><span v-html="$t('thank-you.paragraph', 'en')"></span
+              ></base-text>
+
+              <div class="text-center">
+                <base-btn
+                  nuxt
+                  :to="localePath('/', 'en')"
+                  class="mt-5 mb-10 text-h6 px-7 py-7"
+                >
+                  <span v-html="$t('thank-you.back', 'en')"></span>
                 </base-btn>
               </div>
             </v-col>
